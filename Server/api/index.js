f5fc122b581126b72c98b863659761c830590c18
abzhk <abishek.sj@AstraCape>
2026-02-07 20:08:40 +0530

    feat: clean up middleware and update CORS allowed origins

--- Server/api/index.js
+++ Server/api/index.js
@@ -13,7 +13,6 @@ const MONGODB = process.env.MONGODB_URI;
 
 const app = express();
 
-console.log("Router loaded");
 
 // ================= MIDDLEWARE =================
 
@@ -23,10 +22,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "https://admin1step.vercel.app",
-  "https://admin1step.vercel.app/api",
   "https://admin1step-4i4k6e6hm-abisheks-projects-eb26add9.vercel.app",
   "https://admin1step-git-test-abisheks-projects-eb26add9.vercel.app",
-  "https://admin1step-5ov0p7213-abisheks-projects-eb26add9.vercel.app"
 ];
 
 app.use(
@@ -47,15 +44,14 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // ================= ROUTES =================
-// root test
-app.get("/api", (req, res) => {
-  res.send("API root working 🚀");
-});
 
 // your existing APIs
 app.use("/api", router);
 
-
+// root test
+app.get("/api", (req, res) => {
+  res.status(200).send("Backend is live 🚀");
+});
 
 
 // ================= DATABASE =================
