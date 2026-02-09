5527356dfd09610d38fc212bab8e946a27e973a4
abzhk <abishek.sj@AstraCape>
2026-02-09 10:30:19 +0530

    refactor: simplify CORS configuration and clean up code structure

--- Server/api/index.js
+++ Server/api/index.js
@@ -3,18 +3,15 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+ 
 import router from "../routes/route.js";
-
+ 
 dotenv.config();
-
+ 
 const MONGODB = process.env.MONGODB_URI;
-
+ 
 const app = express();
-
-
-app.use(cookieParser());
-
+ 
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
@@ -22,47 +19,38 @@ const allowedOrigins = [
   "https://admin1step-git-test-abisheks-projects-eb26add9.vercel.app",
   "https://admin1step-6hg40tlki-abisheks-projects-eb26add9.vercel.app",
 ];
-
-app.use((req, res, next) => {
-  console.log("PATH RECEIVED:", req.originalUrl);
-  next();
-});
-
-
+ 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
-  })
+  }),
 );
-app.options("*", cors());
+ 
+ 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
-app.use("/api", router);
-
-
-app.get("/api", (req, res) => {
-  res.status(200).send("Backend is live ");
-});
-
-
-
+ 
 if (!mongoose.connections[0].readyState) {
   mongoose
     .connect(MONGODB)
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch((error) =>
-      console.error("Error connecting to MongoDB:", error.message)
+      console.error("Error connecting to MongoDB:", error. Message)
     );
 }
-
-
+ 
+ 
+app.use("/api", router);
+app.get("/api", (req, res) => {
+  res.status(200).send("Backend is live ");
+});
+app.use((req, res, next) => {
+  console.log("PATH RECEIVED:", req.originalUrl);
+  next();
+});
+ 
+ 
+ 
 export default app;
