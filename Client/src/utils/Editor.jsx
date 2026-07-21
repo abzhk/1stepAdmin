7ceeae12528c602544c901f1ad5b74c5dfd209a8
abzhk <abisheksj905@gmail.com>
2026-07-21 16:21:53 +0530

    refactor:"html quill updated for editor text"

--- Client/src/utils/Editor.jsx
+++ Client/src/utils/Editor.jsx
@@ -7,26 +7,46 @@ const Editor = ({ value, onChange }) => {
   const quillRef = useRef(null);
 
   useEffect(() => {
-    if (!quillRef.current) {
-      quillRef.current = new Quill(editorRef.current, {
-        theme: "snow",
-        modules: {
-          toolbar: [
-            [{ header: [1, 2, 3, false] }],
-            ["bold", "italic"],
-            [{ list: "ordered" }, { list: "bullet" }],
-            ["link"],
-          ],
-        },
+    if (quillRef.current) return;
+
+    quillRef.current = new Quill(editorRef.current, {
+      theme: "snow",
+      modules: {
+        toolbar: [
+          [{ header: [1, 2, 3, false] }],
+          ["bold", "italic"],
+          [{ list: "ordered" }, { list: "bullet" }],
+          ["link"],
+        ],
+      },
+    });
+
+    // Remove unwanted formatting from pasted content
+    quillRef.current.clipboard.addMatcher(Node.ELEMENT_NODE, (node, delta) => {
+      delta.ops.forEach((op) => {
+        if (op.attributes) {
+          delete op.attributes.color;
+          delete op.attributes.background;
+          delete op.attributes.font;
+          delete op.attributes.size;
+        }
       });
 
-      quillRef.current.on("text-change", () => {
-        onChange(quillRef.current.root.innerHTML);
-      });
-    }
+      return delta;
+    });
 
-    // set initial value
-    if (value && quillRef.current.root.innerHTML !== value) {
+    quillRef.current.on("text-change", () => {
+      const html = quillRef.current.root.innerHTML;
+      onChange(html);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (
+      quillRef.current &&
+      value !== undefined &&
+      quillRef.current.root.innerHTML !== value
+    ) {
       quillRef.current.root.innerHTML = value;
     }
   }, [value]);
