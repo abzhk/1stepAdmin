6827560dbc05b2893e2c4426f59763fd44ee665b
abzhk <abisheksj905@gmail.com>
2026-08-27 13:08:07 +0530

    refactor: add verifyAdminToken middleware to all routes for enhanced security

--- Server/routes/specialization.route.js
+++ Server/routes/specialization.route.js
@@ -7,24 +7,25 @@ import {
   deleteSpecialization,
   getSpecializationsWithPagination,
 } from "../controller/specialization.controller.js";
+import { verifyAdminToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", createSpecialization);
+router.post("/",verifyAdminToken, createSpecialization);
 
 // GET ALL
-router.get("/", getSpecializations);
+router.get("/",verifyAdminToken, getSpecializations);
 
 // GET WITH PAGINATION
-router.get("/pagination", getSpecializationsWithPagination);
+router.get("/pagination", verifyAdminToken, getSpecializationsWithPagination);
 
 // GET BY ID
-router.get("/:id", getSpecializationById);
+router.get("/:id", verifyAdminToken, getSpecializationById);
 
 // UPDATE
-router.put("/:id", updateSpecialization);
+router.put("/:id", verifyAdminToken, updateSpecialization);
 
 // DELETE
-router.delete("/:id", deleteSpecialization);
+router.delete("/:id", verifyAdminToken, deleteSpecialization);
 
 export default router;
