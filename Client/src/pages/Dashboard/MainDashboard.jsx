0c494ad5f38a840989e58181a2a355bbebc8df90
abzhk <abisheksj905@gmail.com>
2026-08-28 11:40:22 +0530

    refactor:"change the path for help tickets for fetch better"

--- Client/src/pages/Dashboard/MainDashboard.jsx
+++ Client/src/pages/Dashboard/MainDashboard.jsx
@@ -49,7 +49,7 @@ const loadDashboard = async () => {
       api("/api/subscription/getcount"),
       api("/api/booking/sessions/count"),
       api("/api/track/monthly"),
-      api("/api/help/dashboard-tickets"),
+      api("/api/help/all-tickets"),
       api("/api/subscription/expired?days=0"),
     ]);
 
